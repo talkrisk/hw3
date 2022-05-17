516bec7fe1c04e37a8759af8eaf1bdf004729d59
talkrisk <sam.cooper@kellogg.northwestern.edu>
2022-05-17 02:09:53 +0000

    Phase 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,23 @@
 class PostsController < ApplicationController
     #def functions
     
-      def new
-        @place = Place.new
+    def new
+        @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        @post["place_id"] = @place["id"]
+      end
      
-    end
+    
 
-    def create
-        @place = Place.new
-        @place["name"] = params["place"]["name"]
-        @place["city"] = params["place"]["city"]
-        @place["state"] = params["place"]["state"]
-        @place.save
-        redirect_to "/places"
+    
+        def create
+            @post = Post.new
+            @post["title"] = params["post"]["title"]
+            @post["description"] = params["post"]["description"]
+            @post["posted_on"] = params["post"]["posted_on"]
+            @post["place_id"] = params["post"]["place_id"]
+            @post.save
+            redirect_to "/places/#{@post["place_id"]}"
       end
     
      
